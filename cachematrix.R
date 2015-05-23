8a716b17a83017dc9a2ea6df7f279dc530542c6b
tonigo <amocenigo001@AMOCENIGO00194.us.nam.ad.pwcinternal.com>
2015-05-23 15:54:54 -0400

    committing cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  ##creates the special matrix
+  
+  get <- function() x
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  list (set = set, get = get,
+        setmatrix = setmatrix,
+        getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...){
+  m <- x$getmatrix()
+  if (!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix <- x$get()
+  m <- solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
+##computes the inverse of the special matrix
+
+a <- makeCacheMatrix()
+## Store a function in a variable a
+
+a$set(matrix(1:4,2,2))
+## stores a matrix
+
+cacheSolve(a)
+## provides result
